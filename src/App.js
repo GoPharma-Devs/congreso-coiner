f590cddcfd4f1bc427c4c4c4684356e5c389977b
Carlos Boyzo <50590301+crbzxy@users.noreply.github.com>
2022-11-18 14:52:08 -0600

    act3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   var signatureEndpoint = 'https://signature-coiner-production.up.railway.app/';
   // This Sample App has been updated to use SDK App type credentials https://marketplace.zoom.us/docs/guides/build/sdk-app
   var sdkKey = 'HXw26mDaKLVcZEYs0kmaZNztcx2wR0IHo2g9';
-  var meetingNumber = '81145266969';
+  var meetingNumber = '83612664820';
   var role = 0;
   var leaveUrl = 'https://congreso.coiner.org/';
   var userName = 'Host';
